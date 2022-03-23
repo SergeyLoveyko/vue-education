8e6759bd68a6aab65756e2ed6d277eedf4eb8330
Sergey <loveykosergey@ukr.net>
2022-03-23 02:41:55 +0200

    feat: 1_VB - lesson_2 - first functional: end

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-Vue.createApp();
+const App = {
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+
+// const app = Vue.createApp(App);      //  Первый способ
+// app.mount('#app');
+
+Vue.createApp(App).mount('#app');       //  Второй способ
