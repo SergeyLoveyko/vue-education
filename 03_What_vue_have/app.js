e1a1bd5a077519b7c9d5bf332e65acf33568ba75
Sergey <loveykosergey@ukr.net>
2022-04-14 23:34:13 +0300

    fead: 2_WVh - lesson_4 - cycle with 'v-for': v-for with object

--- 03_What_vue_have/app.js
+++ 03_What_vue_have/app.js
@@ -1,6 +1,11 @@
 Vue.createApp({
   data: () => ({
     myHtml: '<h1>Vue 3 App</h1>',
-    title: 'Я есть Грут'
+    title: 'Я есть Грут',
+    person: {
+      firstName: 'Kolya',
+      lastName: 'Sorokin',
+      age: 31
+    }
   })
 }).mount('#app');
