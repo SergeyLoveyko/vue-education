c4e5ea6c6d3d9c5e435425c67690d525ab317409
Sergey <loveykosergey@ukr.net>
2022-04-20 00:45:19 +0300

    fead: 3_WVw - lesson_2 - virtual DOM and render: start

--- 04_How_vue_works/app.js
+++ 04_How_vue_works/app.js
@@ -1,15 +1,18 @@
+const h = Vue.h;
+
 const app = Vue.createApp({
   data() {
     return {
       title: 'Это из свойства template'
     }
   },
-  template: `
-    <div class="card center">
-      <h1>{{ title }}</h1>
-      <button class="btn" @click="title = 'Новое название!'">Изменить</button>
-    </div>
-    `
+  // template: `
+  //   <div class="card center">
+  //     <h1>{{ title }}</h1>
+  //     <button class="btn" @click="title = 'Новое название!'">Изменить</button>
+  //   </div>
+  // `
+  
 });
 
 app.mount('#app');
