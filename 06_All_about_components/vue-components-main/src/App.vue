eab6ced900805f9088b3d435dbc65c6d2c50b80c
Sergey <loveykosergey@ukr.net>
2022-05-03 02:52:58 +0300

    fead: 6_AoC - lesson_3 - why use components: flag opening exaple

--- 06_All_about_components/vue-components-main/src/App.vue
+++ 06_All_about_components/vue-components-main/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    <h1>This is title</h1>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Актуальные новости {{ now }}</h2>
+    </div>
+
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{ item }}</h3>
+      <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+      <p v-if="isOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto perferendis, dolor doloribus rem mollitia.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      isOpen: false,
+      news: [
+        'Джо Байден победил на выборах в США',
+        'Vue 3 успешно работает'
+      ]
+    }
   }
 }
 </script>
