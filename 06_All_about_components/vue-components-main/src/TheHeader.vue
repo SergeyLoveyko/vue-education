aea323460a701bff055491132f683531b920153f
Sergey <loveykosergey@ukr.net>
2022-05-03 02:31:57 +0300

    fead: 6_AoC - lesson_2 - how components work

---
+++ 06_All_about_components/vue-components-main/src/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>{{ title }}</h2>
+  <hr />
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maxime vitae illum molestiae nobis corporis exercitationem animi at nostrum quaerat, esse debitis molestias quam dicta. Officia eaque impedit voluptatem qui?</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'The Header from Data'
+    }
+  }
+}
+</script>
