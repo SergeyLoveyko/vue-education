429f79267fe5aedad88b3349ac19a8b1e0afc0cf
Sergey <loveykosergey@ukr.net>
2022-05-05 23:46:46 +0300

    fead: 6_AoC - lesson_9 - example, add logic

--- 06_All_about_components/vue-components-main/src/App.vue
+++ 06_All_about_components/vue-components-main/src/App.vue
@@ -2,7 +2,9 @@
   <div class="container pt-1">
     <div class="card">
       <h2>Актуальные новости {{ now }}</h2>
-      <span>Открыть: {{ openRate }}</span>
+      <span>Открыть: 
+        <strong>{{ openRate }}</strong> | Прочитано: <strong>{{ readRate }}</strong>
+      </span>
     </div>
 
     <app-news
@@ -11,7 +13,10 @@
         :title="item.title"
         :id="item.id"
         :is-open="item.isOpen"
+        :was-read="item.wasRead"
         @open-news="openNew"
+        @read-news="readNews"
+        @unmark="unreadNews"
     ></app-news>
   </div>
 </template>
@@ -23,16 +28,19 @@ export default {
     return {
       now: new Date().toLocaleDateString(),
       openRate: 0,
+      readRate: 0,
       news: [
         {
           title: 'Джо Байден победил на выборах в США',
           id: 1,
-          isOpen: false
+          isOpen: false,
+          wasRead: false
         },
         {
           title: 'Vue 3 успешно работает',
           id: 2,
-          isOpen: false
+          isOpen: false,
+          wasRead: false
         }
       ]
     }
@@ -40,6 +48,16 @@ export default {
   methods: {
     openNew() {
       this.openRate++
+    },
+    readNews(id) {
+      const idx = this.news.findIndex(news => news.id === id)
+      this.news[idx].wasRead = true
+      this.readRate++
+    },
+    unreadNews(id) {
+      const news = this.news.find(news => news.id === id)
+      news.wasRead = false
+      this.readRate--
     }
   },
   components: {
--- 06_All_about_components/vue-components-main/src/AppNews.vue
+++ 06_All_about_components/vue-components-main/src/AppNews.vue
@@ -2,9 +2,19 @@
   <div class="card">
     <h3>{{ title }}</h3>
     <button class="btn" @click="open">
-      {{ isNewOpen ? 'Закрыть' : 'Открыть' }}
+      {{ isNewOpen ? "Закрыть" : "Открыть" }}
     </button>
-    <p v-if="isNewOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto perferendis, dolor doloribus rem mollitia.</p>
+    <button class="btn danger" v-if="wasRead" @click="unmark">
+      Отметить не прочитанное
+    </button>
+    <div v-if="isNewOpen">
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto
+        perferendis, dolor doloribus rem mollitia.
+      </p>
+      <button v-if="!wasRead" class="btn primary" @click="mark">Прочесть новость</button>
+    </div>
   </div>
 </template>
 
@@ -12,48 +22,64 @@
 export default {
   // props: ['title'],
   props: {
+    wasRead: Boolean,
     title: {
       type: String,
-      required: true,  // можем установить, является ли 'title' обязательным параметром
+      required: true, // можем установить, является ли 'title' обязательным параметром
     },
     id: {
       type: Number,
-      required: true
+      required: true,
     },
     isOpen: {
       type: Boolean,
-      required: false,  //  если параметр не обязательный, задаём дефолтный (ниже)
+      required: false, //  если параметр не обязательный, задаём дефолтный (ниже)
       default: false,
       validator(value) {
         // console.log(value)
         // return false
-        return value === true || value === false
-      }
-    }
+        return value === true || value === false;
+      },
+    },
   },
   // emits: ['open-news'],    //  нужен для других разработчиков
   emits: {
-    // 'open-news': null      //  если не хотим валидировать
-    'open-news'(num) {
-      if (num) {
+    'open-news': null,      //  если не хотим валидировать
+    'read-news'(id) {
+      if (id) {
         return true
       }
-      console.warn('No data in open-news emit')
+      console.warn('Нет параметра id для emit read-news')
       return false
-    }
+    },
+    unmark: null
+    // "open-news"(num) {
+    //   if (num) {
+    //     return true;
+    //   }
+    //   console.warn("No data in open-news emit");
+    //   return false;
+    // },
   },
   data() {
     return {
-      isNewOpen: this.isOpen
-    }
+      isNewOpen: this.isOpen,
+    };
   },
   methods: {
     open() {
-      this.isNewOpen = !this.isNewOpen
-      if(this.isNewOpen) {
-        this.$emit('open-news')      //  Минимум 1 аргумент до бусконечности
+      this.isNewOpen = !this.isNewOpen;
+      if (this.isNewOpen) {
+        this.$emit("open-news"); //  Минимум 1 аргумент до бусконечности
       }
+    },
+    mark() {
+      this.isNewOpen = false
+      this.$emit('read-news', this.id)
+    },
+    unmark() {
+      this.$emit('unmark', this.id)
     }
-  }
-}
+  },
+};
 </script>
